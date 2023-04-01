830a72c5d3c9dcf67e026432b38f56d789a15080
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-04-01 12:30:08 +0530

    fix: import and type checks

--- deploy/02-update-front-end.ts
+++ deploy/02-update-front-end.ts
@@ -1,29 +1,30 @@
-import { frontEndContractsFile } from "../helper-hardhat-config"
-import fs from "fs"
-import {DeployFunction} from "hardhat-deploy/types"
-import {HardhatRuntimeEnvironment} from "hardhat/types"
+import { frontEndContractsFile, frontEndAbiFile } from '../helper-hardhat-config'
+import fs from 'fs'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
-const updateUI: DeployFunction = async function (
-    hre: HardhatRuntimeEnvironment
-  ) {
+const updateUI: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { network, ethers } = hre
-    const chainId = "31337"
-    
+    const chainId = '31337'
+
     if (process.env.UPDATE_FRONT_END) {
-        console.log("Writing to front end...")
-        const fundMe = await ethers.getContract("Raffle")
-        const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
+        console.log('Writing to front end...')
+        const raffle = await ethers.getContract('Raffle')
+        const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, 'utf8'))
         if (chainId in contractAddresses) {
-            if (!contractAddresses[network.config.chainId!].includes(fundMe.address)) {
-                contractAddresses[network.config.chainId!].push(fundMe.address)
+            if (!contractAddresses[network.config.chainId!].includes(raffle.address)) {
+                contractAddresses[network.config.chainId!].push(raffle.address)
             }
         } else {
-            contractAddresses[network.config.chainId!] = [fundMe.address]
+            contractAddresses[network.config.chainId!] = [raffle.address]
         }
         fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
-        fs.writeFileSync(frontEndAbiFile, raffle.interface.format(ethers.utils.FormatTypes.json))
-        console.log("Front end written!")
+        fs.writeFileSync(
+            frontEndAbiFile,
+            raffle.interface.format(ethers.utils.FormatTypes.json) as string
+        )
+        console.log('Front end written!')
     }
 }
 export default updateUI
-updateUI.tags = ["all", "frontend"]
+updateUI.tags = ['all', 'frontend']
